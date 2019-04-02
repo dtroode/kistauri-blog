84f5c44309eefe1e75ec36730ca28f6364e20646
David <d.kistauri2014@gmail.com>
2019-04-02 22:56:11 +0300

    PWA Improvments

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+var cacheName = 'Kistauri';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/main.js',
+  '/styles/main.css',
+  '/images/bg.jpg',
+  '/fonts/Inter-UI-Bold.woff2',
+  '/fonts/Inter-UI-Medium.woff2',
+  '/fonts/Inter-UI-MediumItalic.woff2'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
