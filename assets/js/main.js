7f8349707e12678d19dc62850d8d07903f91a426
dtroode <d.kistauri2014@gmail.com>
2019-05-27 14:37:16 +0300

    Changes in elements position

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,3 @@ if ('serviceWorker' in navigator) {
            .register('/service-worker.js')
            .then(function() { console.log('You can use this site without internet! ⚡'); });
 }
-
-let tagsButtonClicked = false;
-
-function openTagsList(button) {
-  if (tagsButtonClicked == false) {
-    button.classList.add('tags-name');
-    tagsButtonClicked = true;
-  } else {
-    button.classList.remove('tags-name');
-    tagsButtonClicked = false;
-  }
-}
