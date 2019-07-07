2a04b5836c6ea823023f7ffe6bd02a608fe8e283
dtroode <davidkis113@gmail.com>
2019-07-07 15:37:22 +0300

    Revert previous changes

--- src/cms/cms.js
+++
@@ -1,9 +0,0 @@
-import CMS from "netlify-cms-app";
-
-import "../styles/fonts-styles/mono.scss";
-import "../styles/pages-styles/post.scss";
-import "../styles/media.scss";
-import "../styles/code.scss";
-import Post from "../templates/blog-post";
-
-CMS.registerPreviewTemplate("blog", Post);
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,7 +7,7 @@ import "../styles/media.scss";
 import "../styles/code.scss";
 import SEO from "../components/seo";
 
-const Post = ({ data }) => {
+export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <Layout
@@ -55,5 +55,3 @@ export const query = graphql`
     }
   }
 `;
-
-export default Post;
