d14a80841d08c89fca9c9783bfdf28f42c8648f4
dtroode <davidkis113@gmail.com>
2019-07-25 00:00:58 +0300

    Change design of portfolio works

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,116 +52,122 @@ const IndexPage = () => {
         <ol>
           <li>
             <a href="https://dtroode.netlify.com/blog">Блог</a> — 2019
-            <br />
-            <span>
-              Сделал себе блог: задизайнил, написал, продолжаю писать.
-            </span>
+            <ul>
+              <li>
+                <strong>Как никто</strong>
+                <br />
+                Сделал себе блог: задизайнил, написал, продолжаю писать.
+              </li>
+            </ul>
           </li>
           <hr />
           <li>
             <a href="https://masterbuket.com">Мастер Букет</a> — 2018
-            <section className="section--table">
-              <section>
-                <strong>Как разработчик</strong>
-                <span>Написал все страницы сайта</span>
-                <span>Выложил сайт на хостинг</span>
-                <span> Администрирую сайт</span>
-              </section>
-              <section>
-                <strong>Как редактор</strong>
-                <span>Написал текст</span>
-                <span>Занимался соц. сетями</span>
-              </section>
-              <section>
+            <ul>
+              <li>
                 <strong>Как руководитель</strong>
-                <span>Спланировал проект</span>
-              </section>
-              <section>
+                <br />
+                Спланировал проект
+              </li>
+              <li>
+                <strong>Как разработчик</strong>
+                <br />
+                Написал все страницы сайта, выложил сайт на хостинг,
+                администрирую сайт
+              </li>
+              <li>
                 <strong>Как дизайнер</strong>
-                <span>Придумал дизайн бренда</span>
-                <span>Придумал дизайн сайта</span>
-              </section>
-            </section>
+                <br />
+                Придумал дизайн бренда, придумал дизайн сайта
+              </li>
+              <li>
+                <strong>Как редактор</strong>
+                <br />
+                Написал текст, занимался соц. сетями
+              </li>
+            </ul>
           </li>
           <hr />
           <li>
             <a href="https://constantvalues.tk">Сайт Постоянные Величины</a> —
             2018
-            <section className="section--table">
-              <section>
-                <strong>Как разработчик</strong>
-                <span>Написал все страницы сайта</span>
-                <span>Выложил сайт на хостинг</span>
-                <span>Администрирую сайт</span>
-              </section>
-              <section>
-                <strong>Как редактор</strong>
-                <span>Написал текст</span>
-              </section>
-              <section>
+            <ul>
+              <li>
                 <strong>Как руководитель</strong>
-                <span>Придумал проект</span>
-                <span>Спланировал проект</span>
-              </section>
-              <section>
+                <br />
+                Придумал проект, спланировал проект
+              </li>
+              <li>
+                <strong>Как разработчик</strong>
+                <br />
+                Написал все страницы сайта, выложил сайт на хостинг,
+                администрирую сайт
+              </li>
+              <li>
                 <strong>Как дизайнер</strong>
-                <span>Придумал дизайн</span>
-              </section>
-            </section>
+                <br />
+                Придумал дизайн
+              </li>
+              <li>
+                <strong>Как редактор</strong>
+                <br />
+                Написал текст
+              </li>
+            </ul>
           </li>
           <hr />
           <li>
             <a href="https://secondschool.gq">Сайт второй школы</a> — 2018
-            <section className="section--table">
-              <section>
-                <strong>Как разработчик</strong>
-                <span>Написал все страницы сайта</span>
-                <span>Выложил сайт на хостинг</span>
-                <span>Администрировал сайт</span>
-              </section>
-              <section>
-                <strong>Как редактор</strong>
-                <span>Отредактировал текст об истории</span>
-                <span>Написал остальной текст</span>
-              </section>
-              <section>
+            <ul>
+              <li>
                 <strong>Как руководитель</strong>
-                <span>Придумал проект</span>
-                <span>Спланировал проект</span>
-                <span>Собрал весь материал</span>
-              </section>
-              <section>
+                <br />
+                Придумал проект, спланировал проект, собрал весь материал
+              </li>
+              <li>
+                <strong>Как разработчик</strong>
+                <br />
+                Написал все страницы сайта, выложил сайт на хостинг,
+                администрировал сайт
+              </li>
+              <li>
                 <strong>Как дизайнер</strong>
-                <span>Придумал редизайн</span>
-              </section>
-            </section>
+                <br />
+                Придумал редизайн
+              </li>
+              <li>
+                <strong>Как редактор</strong>
+                <br />
+                Отредактировал текст об истории, написал остальной текст
+              </li>
+            </ul>
           </li>
           <hr />
           <li>
             <a href="https://cyxym.cf">Сайт CYXYM</a> — 2018
-            <section className="section--table">
-              <section>
-                <strong>Как разработчик</strong>
-                <span>Написал все страницы сайта</span>
-                <span>Выложил сайт на хостинг</span>
-                <span>Администрирую сайт</span>
-              </section>
-              <section>
-                <strong>Как редактор</strong>
-                <span>Написал текст о местах и людях</span>
-                <span>Отредактировал текст об истории</span>
-              </section>
-              <section>
+            <ul>
+              <li>
                 <strong>Как руководитель</strong>
-                <span>Придумал проект</span>
-                <span>Спланировал проект</span>
-                <span>Руководил командой</span>
-              </section>
-              <section>
+                <br />
+                Придумал проект, спланировал проект, руководил командой
+              </li>
+              <li>
+                <strong>Как разработчик</strong>
+                <br />
+                Написал все страницы сайта, выложил сайт на хостинг,
+                администрирую сайт
+              </li>
+              <li>
                 <strong>Как дизайнер</strong>
-                <span>Придумал дизайн</span>
-              </section>
-            </section>
+                <br />
+                Придумал дизайн
+              </li>
+              <li>
+                <strong>Как редактор</strong>
+                <br />
+                Написал текст о местах и людях, отредактировал текст об истории
+              </li>
+            </ul>
           </li>
         </ol>
       </section>
