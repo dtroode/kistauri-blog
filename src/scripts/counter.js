153f9dbcef80dc295bd971910c9095570fa59d68
dtroode <davidkis113@gmail.com>
2019-08-20 15:57:14 +0300

    Rewrite counter

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -1,15 +1,12 @@
 function counter(number) {
   const names = ["пост", "поста", "постов"];
-  const twoFour = ["2", "3", "4"];
-  const twentyFourteen = ["12", "13", "14"];
-  const numberStringified = number.toString();
   return names[
-    numberStringified.endsWith("1")
-      ? numberStringified.endsWith("11")
+    /1$/.test(number)
+      ? /[1][1]$/.test(number)
         ? 2
         : 0
-      : twoFour.some(n => numberStringified.endsWith(n))
-      ? twentyFourteen.some(n => numberStringified.endsWith(n))
+      : /(2|3|4)$/.test(number)
+      ? /([1][2]|[1][3]|[1][4])$/.test(number)
         ? 2
         : 1
       : 2
