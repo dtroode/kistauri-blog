068a854e9136c9b5c77323608b82865c06c6ee5b
dtroode <davidkis113@gmail.com>
2019-08-29 21:01:42 +0300

    Remove Preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,6 @@ module.exports = {
     blogUrl: `https://davidkistauri.now.sh/blog/`
   },
   plugins: [
-    `gatsby-plugin-preact`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
